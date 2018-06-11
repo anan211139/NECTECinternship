33338e6ec2d512bbf22e1d1dbb17ad897e70292b
anan211139 <parichaya211139@gmail.com>
2018-06-11 17:03:45 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'tick'
+				'text' => 'Intern'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
