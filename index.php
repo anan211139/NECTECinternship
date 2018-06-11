3be6df3ed1b56590ebaafb7aa7c70d8b5a1e06bd
anan211139 <parichaya211139@gmail.com>
2018-06-11 16:44:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => 'test'
+				'type' => "text",
+				'text' => "test"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
