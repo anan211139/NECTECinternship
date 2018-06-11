d54150fbbbe625e301be8260a0a75a418c3c18b6
anan211139 <parichaya211139@gmail.com>
2018-06-11 17:02:49 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -43,7 +43,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
+			
+			echo $result. "\r\n";
 			
 			
 			
