662674e5c3acf70e03fa5c3d8fe93a2f5c272c80
anan211139 <parichaya211139@gmail.com>
2018-06-11 16:23:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "บ้อบอ"
+				'text' => 'บ้อบอ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
