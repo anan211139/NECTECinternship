2084960a02a6ce5094be7e6654f0833d848c7e50
parewari <maroengsit.pwr@gmail.com>
2018-06-12 16:24:54 +0700

    latest

--- webhook.php
+++ webhook.php
@@ -46,14 +46,14 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-			
-			
+
+
 			echo $result. "\r\n";
-			
-			
-			
+
+
+
 		}
-		
+
 	}
 }
 echo "OK";
