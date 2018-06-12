99523bca7cb7fd25e1b5242e1ba4ac9c77f9d35a
anan211139 <parichaya211139@gmail.com>
2018-06-12 08:32:13 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -16,7 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['source']['userId'];
+			//$text = $event['source']['userId'];
+			$text = $event[0]['message']['text'];
 			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
