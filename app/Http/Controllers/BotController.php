8064d2a0c483b4065576823ad6d3c63a0b62d98b
Chayanin <chayanin2007@gmail.com>
2018-06-13 16:08:43 +0700

    edit BotController

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -8,8 +8,6 @@ class BotController extends Controller
 {
     public function index() {
         echo "iitim";
-        require "vendor/autoload.php";
-        require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
         $access_token = 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEPeF2D//vC4kHTLQuQGbgpZSqU3C+WUJ86nQNptlraZZtek2tdLYoqREXuN8xy3swo9RVO3EL0VrmnhSQfuOl89AQdB04t89/1O/w1cDnyilFU=';
         // Get POST body content
         $content = file_get_contents('php://input');
