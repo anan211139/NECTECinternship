d5a3de48c879f1c785c7113da6c904c767ce8229
anan211139 <parichaya211139@gmail.com>
2018-06-13 17:25:00 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -50,20 +50,52 @@ $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SE
 $content = file_get_contents('php://input');
  
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+// $events = json_decode($content, true);
+// if(!is_null($events)){
+//     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+//     $replyToken = $events['events'][0]['replyToken'];
+// }
+// // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+// $textMessageBuilder = new TextMessageBuilder(json_encode($events));
+ 
+// //l ส่วนของคำสั่งตอบกลับข้อความ
+// $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+// if ($response->isSucceeded()) {
+//     echo 'Succeeded!';
+//     return;
+// }
+ 
+// // Failed
+// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
 $events = json_decode($content, true);
 if(!is_null($events)){
     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
     $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "A":
+                    $textReplyMessage = "คุณพิมพ์ A";
+                    break;
+                case "B":
+                    $textReplyMessage = "คุณพิมพ์ B";
+                    break;
+                default:
+                    $textReplyMessage = " คุณไม่ได้พิมพ์ A และ B";
+                    break;                                      
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            break;  
+    }
 }
 // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
-$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
  
 //l ส่วนของคำสั่งตอบกลับข้อความ
 $response = $bot->replyMessage($replyToken,$textMessageBuilder);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
- 
-// Failed
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
