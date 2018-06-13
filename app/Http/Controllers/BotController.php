9561c9ad33cafb5ff9e6f594f1304e1f6d26d442
Chayanin <chayanin2007@gmail.com>
2018-06-13 21:53:55 +0700

    add echo checkpoint for debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -48,16 +48,22 @@ class BotController extends Controller
         // เชื่อมต่อกับ LINE Messaging API
         $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
         $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+        echo "A";
         
         // คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
         $content = file_get_contents('php://input');
+        echo "B";
         echo $content;
         
         // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
         $events = json_decode($content, true);
+        echo "C";
+        echo $events;
         if(!is_null($events)){
             // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
             $replyToken = $events['events'][0]['replyToken'];
+            echo "D";
+            echo $replyToken;
         }
         // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
         $textMessageBuilder = new TextMessageBuilder(json_encode($events));
