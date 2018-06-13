a5836de324a2c409b719eda6d5cf848e0cd1c42f
Chayanin <chayanin2007@gmail.com>
2018-06-13 21:28:50 +0700

    bring back old func

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('bot', 'BotController@indextwo');
+Route::get('bot', 'BotController@index');
