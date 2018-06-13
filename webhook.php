74b1b5c2476186c323eebd515defdd43cf4279f2
anan211139 <parichaya211139@gmail.com>
2018-06-13 09:39:16 +0700

    Update webhook.php
    test first greeting reply

--- webhook.php
+++ webhook.php
@@ -11,6 +11,46 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
+	if (!is_null($events['events'][0])){
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			//$text = $event['source']['userId'];
+			$text = $event['message']['text'];
+			//$text = $event['message']['text'];
+			
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => "สวัสดีจ้าน้อง".$text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+
+			echo $result. "\r\n";
+
+		}
+		
+	}
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -26,7 +66,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "สวัสดีจ้าน้อง".$text
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
