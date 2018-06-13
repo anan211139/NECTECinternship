8517fb68aed9c6d44bd549403e42a677cb3e4221
parewari <maroengsit.pwr@gmail.com>
2018-06-13 13:25:57 +0700

    update same as master

--- webhook.php
+++ webhook.php
@@ -1,10 +1,7 @@
 <?php // callback.php
-
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
 $access_token = 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEPeF2D//vC4kHTLQuQGbgpZSqU3C+WUJ86nQNptlraZZtek2tdLYoqREXuN8xy3swo9RVO3EL0VrmnhSQfuOl89AQdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,26 +13,67 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['source']['userId'];
 			$text = $event['message']['text'];
-			//$text = $event['message']['text'];
-
-
-			if($text=='สวัสดี'){
-				$text = 'สวัสดีค่า';
-			}else{
-				$text = '555555';
-			}
-
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
+			
+// 			if($text == 'เปลี่ยนวิชา'){
+// 				$text = 'กรุณาระบุวิชา';
+// 			}
+			
+// 			$messages = [
+// 					'type' => 'text',
+// 					'text' => $text
+// 				];
+			if($text == 'เปลี่ยนวิชา'){
+				
+				$messages = [
+					'type' => 'imagemap',
+					'baseUrl' => 'https://github.com/anan211139/NECTECinternship/blob/master/img/sybject.jpg?raw=true',
+					'altText' => 'เปลี่ยนวิชา',
+					'baseSize' => 
+					array (
+						'width' => 1040,
+						'height' => 513,
+					),
+					'actions' => 
+					array (
+    						0 => 
+    						array (
+      						'type' => 'message',
+      						'area' => 
+      							array (
+        							'x' => 43,
+        							'y' => 190,
+        							'width' => 954,
+        							'height' => 107,
+      							),
+      						'text' => 'วิชาคณิตศาตร์',
+    						),
+    						1 => 
+    						array (
+      							'type' => 'message',
+      							'area' => 
+      							array (
+        							'x' => 39,
+        							'y' => 311,
+        							'width' => 960,
+        							'height' => 105,
+      							),
+      						'text' => 'วิชาภาษาอังกฤษ',
+    						),
+  					),'text' => $text
+				];
+			}
+			else{
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+			}
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -44,7 +82,6 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -53,14 +90,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
-
 			echo $result. "\r\n";
-
-
-
 		}
-
 	}
 }
 echo "OK";
