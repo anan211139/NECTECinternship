54e04ec84ad1a51a98c5d57a94eac7275ac52029
anan211139 <parichaya211139@gmail.com>
2018-06-13 16:40:59 +0700

    Update webhook.php
    change to linebot-sdk

--- webhook.php
+++ webhook.php
@@ -3,170 +3,62 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEPeF2D//vC4kHTLQuQGbgpZSqU3C+WUJ86nQNptlraZZtek2tdLYoqREXuN8xy3swo9RVO3EL0VrmnhSQfuOl89AQdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+//use LINE\LINEBot\Event;
+//use LINE\LINEBot\Event\BaseEvent;
+//use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use LINE\LINEBot\ImagemapActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder ;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
+ 
+// เชื่อมต่อกับ LINE Messaging API
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+ 
+// คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
 $content = file_get_contents('php://input');
-// Parse JSON
+ 
+// แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			// Build message to reply back
-			
-// 			if($text == 'เปลี่ยนวิชา'){
-// 				$text = 'กรุณาระบุวิชา';
-// 			}
-			
-// 			$messages = [
-// 					'type' => 'text',
-// 					'text' => $text
-// 				];
-			if($text == 'เปลี่ยนวิชา'){
-				
-				$messages = [
-					'type' => 'imagemap',
-					'baseUrl' => 'https://github.com/anan211139/NECTECinternship/blob/master/img/Sub.jpg?raw=true',
-					'altText' => 'เปลี่ยนวิชา',
-					'baseSize' => 
-					array (
-						'width' => 1040,
-						'height' => 530,
-					),
-					'actions' => 
-					array (
-    						0 => 
-    						array (
-      						'type' => 'message',
-      						'area' => 
-      							array (
-        							'x' => 41,
-        							'y' => 173,
-        							'width' => 956,
-        							'height' => 137,
-      							),
-      						'text' => 'วิชาคณิตศาตร์',
-    						),
-    						1 => 
-    						array (
-      							'type' => 'message',
-      							'area' => 
-      							array (
-        							'x' => 43,
-        							'y' => 314,
-        							'width' => 954,
-        							'height' => 137,
-      							),
-      						'text' => 'วิชาภาษาอังกฤษ',
-    						),
-  					),'text' => $text
-				];
-			}
-// 			else if($text == 'เปลี่ยนหัวข้อ'||$text == 'วิชาคณิตศาสตร์'){
-// 				$messages = [
-// 					'type' => 'imagemap',
-// 					'baseUrl' => 'https://github.com/anan211139/NECTECinternship/blob/master/img/lesson.jpg?raw=true',
-// 					'altText' => 'This is an imagemap',
-// 					'baseSize' => 
-//   						array (
-//     							'width' => 1040,
-//     							'height' => 750,
-//   						),
-//   					'actions' => 
-//   					array (
-//     					0 => 
-//     						array (
-// 							'type' => 'message',
-// 							'area' => 
-// 								array (
-// 									'x' => 41,
-// 									'y' => 204,
-// 									'width' => 956,
-// 									'height' => 110,
-// 								),
-// 							'text' => 'สมการ',
-//     						),
-//     					1 => 
-//     						array (
-//       							'type' => 'message',
-//       							'area' => 
-//       								array (
-//         								'x' => 41,
-//         								'y' => 322,
-//         								'width' => 956,
-//         								'height' => 109,
-//       								),
-//       							'text' => 'หรม./ครน.',
-//     						),
-//     					2 => 
-//     						array (
-//       							'type' => 'message',
-//       							'area' => 
-// 								array (
-// 									'x' => 39,
-// 									'y' => 442,
-// 									'width' => 958,
-// 									'height' => 109,
-// 								),
-//       							'text' => 'โจทย์ปัญหา',
-//     						),
-//     					3 => 
-//     						array (
-//       							'type' => 'message',
-//       							'area' => 
-//       								array (
-//         								'x' => 41,
-//         								'y' => 572,
-//         								'width' => 958,
-//         								'height' => 105,
-//       								),
-//       							'text' => 'ตัวประกอบ',
-//     							),
-//   						),
-// 					)
-// 				];
-// 			}
-			else{
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
-			}
-			
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-
-			echo $result. "\r\n";
-
-
-
-		}
-
-	}
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
 }
-echo "OK";
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder(json_encode($events));
+ 
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
+}
+ 
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
