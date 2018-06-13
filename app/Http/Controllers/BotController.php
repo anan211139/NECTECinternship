79bf24a1927479b6a3446c9a87b57be91c0ad949
Chayanin <chayanin2007@gmail.com>
2018-06-13 21:23:52 +0700

    try another function

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -71,4 +71,66 @@ class BotController extends Controller
         // Failed
         echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
     }
+
+    public function indextwo() {
+        $API_URL = 'https://api.line.me/v2/bot/message/reply';
+        $ACCESS_TOKEN = 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEPeF2D//vC4kHTLQuQGbgpZSqU3C+WUJ86nQNptlraZZtek2tdLYoqREXuN8xy3swo9RVO3EL0VrmnhSQfuOl89AQdB04t89/1O/w1cDnyilFU=';
+        $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+        $request = file_get_contents('php://input');   // Get request content
+        $request_array = json_decode($request, true);   // Decode JSON to Array
+
+        if ( sizeof($request_array['events']) > 0 )
+        {
+
+        foreach ($request_array['events'] as $event)
+        {
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+        if ( $event['type'] == 'message' ) 
+        {
+        if( $event['message']['type'] == 'text' )
+        {
+            $text = $event['message']['text'];
+            $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+        }
+        else
+            $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+        
+        }
+        else
+        $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+        
+        if( strlen($reply_message) > 0 )
+        {
+        //$reply_message = iconv("tis-620","utf-8",$reply_message);
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [['type' => 'text', 'text' => $reply_message]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+        echo "Result: ".$send_result."\r\n";
+        }
+        }
+        }
+
+        echo "OK";
+
+        function send_reply_message($url, $post_header, $post_body)
+        {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+        }
+    }
 }
