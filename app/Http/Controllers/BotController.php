4ddcb5cd5d92908296ecb6afc6b1480968fd9f8a
Chayanin <chayanin2007@gmail.com>
2018-06-14 11:32:20 +0700

    change back

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -40,122 +40,35 @@ define('LINE_MESSAGE_CHANNEL_ID', '1586241418');
 define('LINE_MESSAGE_CHANNEL_SECRET', '40f2053df45b479807d8f2bba1b0dbe2');
 define('LINE_MESSAGE_ACCESS_TOKEN', 'VjNScyiNVZFTg96I4c62mnCZdY6bqyllIaUZ4L3NHg5uObrERh7O5m/tO3bbgEPeF2D//vC4kHTLQuQGbgpZSqU3C+WUJ86nQNptlraZZtek2tdLYoqREXuN8xy3swo9RVO3EL0VrmnhSQfuOl89AQdB04t89/1O/w1cDnyilFU=');
 
-
 class BotController extends Controller
 {
-    
-    private $httpClient     = null;
-    private $endpointBase   = null;
-    private $channelSecret  = null;
-	
-    public $content         = null;
-    public $events          = null;
-	
-    public $isEvents        = false;
-    public $isText          = false;
-    public $isImage         = false;
-    public $isSticker       = false;
-	
-    public $text            = null;
-    public $replyToken      = null;
-    public $source          = null;
-    public $message         = null;
-    public $timestamp       = null;
-	
-    public $response        = null;
-
     public function index() {
         echo "iitim";
-        $this->start(LINE_MESSAGE_CHANNEL_SECRET, LINE_MESSAGE_ACCESS_TOKEN);
-        echo $this->channelSecret;
-        if (!empty($this->isEvents)) {
-		
-            $this->replyMessageNew($this->replyToken, json_encode($this->message));
+        // เชื่อมต่อกับ LINE Messaging API
+        $httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+        $bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
         
-            if ($this->isSuccess()) {
-                echo 'Succeeded!';
-                exit();
-            }
+        // คำสั่งรอรับการส่งค่ามาของ LINE Messaging API
+        $content = file_get_contents('php://input');
         
-            // Failed
-            echo $this->response->getHTTPStatus . ' ' . $this->response->getRawBody(); 
-            exit();
-        
+        // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
+        $events = json_decode($content, true);
+        if(!is_null($events)){
+            // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+            $replyToken = $events['events'][0]['replyToken'];
         }
+        // ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+        $textMessageBuilder = new TextMessageBuilder(json_encode($events));
+        
+        //l ส่วนของคำสั่งตอบกลับข้อความ
+        $response = $bot->replyMessage($replyToken,$textMessageBuilder);
+        if ($response->isSucceeded()) {
+            echo 'Succeeded!';
+            return;
+        }
+        
+        // Failed
+        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
         return "This is my app";
-    }
-    
-    // public function start($channelSecret, $access_token) {
-    //     $this->httpClient     = new CurlHTTPClient($access_token);
-    //     $this->channelSecret  = $channelSecret;
-    //     $this->endpointBase   = LINEBot::DEFAULT_ENDPOINT_BASE;
-		
-    //     $this->content        = file_get_contents('php://input');
-    //     $events               = json_decode($this->content, true);
-		
-    //     if (!empty($events['events'])) {
-			
-    //         $this->isEvents = true;
-    //         $this->events   = $events['events'];
-			
-    //         foreach ($events['events'] as $event) {
-				
-    //             $this->replyToken = $event['replyToken'];
-    //             $this->source     = (object) $event['source'];
-    //             $this->message    = (object) $event['message'];
-    //             $this->timestamp  = $event['timestamp'];
-				
-    //             if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-    //                 $this->isText = true;
-    //                 $this->text   = $event['message']['text'];
-    //             }
-				
-    //             if ($event['type'] == 'message' && $event['message']['type'] == 'image') {
-    //                 $this->isImage = true;
-    //             }
-				
-    //             if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-    //                 $this->isSticker = true;
-    //             }
-				
-    //         }
-    //     }
-		
-    //     parent::start($this->httpClient, [ 'channelSecret' => $channelSecret ]);
-		
-    // }
-	
-    public function sendMessageNew ($to = null, $message = null) {
-        $messageBuilder = new TextMessageBuilder($message);
-        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/push', [
-            'to' => $to,
-            // 'toChannel' => 'Channel ID,
-            'messages'  => $messageBuilder->buildMessage()
-        ]);
-    }
-	
-    public function replyMessageNew ($replyToken = null, $message = null) {
-        $messageBuilder = new TextMessageBuilder($message);
-        $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
-            'replyToken' => $replyToken,
-            'messages'   => $messageBuilder->buildMessage(),
-        ]);
-    }
-	
-    public function isSuccess () {
-        return !empty($this->response->isSucceeded()) ? true : false;
-    }
-	
-    public static function verify ($access_token) {
-		
-        $ch = curl_init('https://api.line.me/v1/oauth/verify');
-		
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [ 'Authorization: Bearer ' . $access_token ]);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($result);
-		
     }
 }
