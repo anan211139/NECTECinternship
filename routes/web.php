6a7143fd61a1d2488c48299f47044d0896a466dc
rawatas <whattheshit@outlook.co.th>
2018-06-15 13:57:38 +0700

    edit web for use controller

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,25 @@
 |
 */
 Route::get('/', 'Pagecontroller@getblankblade');
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::get('/home', 'Pagecontroller@gethome');
-Route::get('/home', function () {
-    return view('home');
-});
-// Route::get('/content', 'Pagecontroller@getcontent');
-Route::get('/content', function () {
-    return view('content');
-});
-// Route::get('/login', 'Pagecontroller@getlogin');
-Route::get('/login', function () {
-    return view('login');
-});
-// Route::get('/regis', 'Pagecontroller@getregis');
-Route::get('/regis', function () {
-    return view('regis');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/home', 'Pagecontroller@gethome');
+// Route::get('/home', function () {
+//     return view('home');
+// });
+Route::get('/content', 'Pagecontroller@getcontent');
+// Route::get('/content', function () {
+//     return view('content');
+// });
+Route::get('/login', 'Pagecontroller@getlogin');
+// Route::get('/login', function () {
+//     return view('login');
+// });
+Route::get('/regis', 'Pagecontroller@getregis');
+// Route::get('/regis', function () {
+//     return view('regis');
+// });
 Route::post('/home/submit', 'MessageController@submit');
 
 Route::resource('Line','LineController');  //call path ที่กำหนด
