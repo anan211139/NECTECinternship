9852ef3abf37e55578c22dc927a31547908cc7ca
anan <parichaya211139@gmail.com>
2018-06-19 09:37:36 +0700

    try qrCODE

--- resources/views/Line/index.blade.php
+++ resources/views/Line/index.blade.php
@@ -1,7 +1,7 @@
 <?php
     echo "anan";
     //------QR CODE-----------
-    $userId="U038940166356c6b9fb0dcf051aded27f";
+    $userId = "U038940166356c6b9fb0dcf051aded27f";
     $_REQUEST['data'] = $userId;
     //set it to writable location, a place for temp generated PNG files
     $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'phpqrcode/temp'.DIRECTORY_SEPARATOR; 
@@ -11,17 +11,16 @@
     //ofcourse we need rights to create temp dir
     if (!file_exists($PNG_TEMP_DIR))
         mkdir($PNG_TEMP_DIR);
-        $filename = $PNG_TEMP_DIR.'test.png';
-        $errorCorrectionLevel = 'L';
-        $matrixPointSize = 4;
+    $filename = $PNG_TEMP_DIR.'test.png';
+    $errorCorrectionLevel = 'L';
+    $matrixPointSize = 4;
     if (isset($_REQUEST['data'])) { 
         //it's very important!
         if (trim($_REQUEST['data']) == '')
             die('data cannot be empty! <a href="?">back</a>');
         // user data
         $filename = $PNG_TEMP_DIR.$_REQUEST['data'].'.png';
-        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2); 
-
+        QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
     } else {    
         //default data
         echo 'You can provide data in GET parameter: <a href="?data=like_that">like that</a><hr/>';    
@@ -30,5 +29,7 @@
     //display generated file
     echo '<img src="'.$PNG_WEB_DIR.basename($filename).'" />';  
     $img_qr = basename($filename);
+    echo $img_qr;
+
 
 ?>
