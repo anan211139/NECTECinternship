9676daea182b57134adb52c3295e0775494367ba
anan <parichaya211139@gmail.com>
2018-06-19 08:40:06 +0700

    test reply qr

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -171,40 +171,49 @@ class BotController extends Controller
                 $arr_replyData = array();
                 $arr_replyData[] = new TextMessageBuilder($userId);
 
-                $_REQUEST['data'] = $userId;
-                //set it to writable location, a place for temp generated PNG files
-                $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR; 
-                //html PNG location prefix
-                $PNG_WEB_DIR = 'phpqrcode/temp/';
-                include "phpqrcode/qrlib.php";    
-                //ofcourse we need rights to create temp dir
-                if (!file_exists($PNG_TEMP_DIR))
-                    mkdir($PNG_TEMP_DIR);
-                $filename = $PNG_TEMP_DIR.'test.png';
-                $errorCorrectionLevel = 'L';
-                $matrixPointSize = 4;
-                if (isset($_REQUEST['data'])) { 
-                    //it's very important!
-                    if (trim($_REQUEST['data']) == '')
-                        die('data cannot be empty! <a href="?">back</a>');
-                    // user data
-                    $filename = $PNG_TEMP_DIR.$_REQUEST['data'].'.png';
-                    QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2); 
+                //------QR CODE-----------
 
-                } else {    
-                    //default data
-                    echo 'You can provide data in GET parameter: <a href="?data=like_that">like that</a><hr/>';    
-                    QRcode::png('PHP QR Code :)', $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
-                }         
-                //display generated file
-                echo '<img src=""phpqrcode/"'.$PNG_WEB_DIR.basename($filename).'" />';  
+                // $_REQUEST['data'] = $userId;
+                // //set it to writable location, a place for temp generated PNG files
+                // $PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'temp'.DIRECTORY_SEPARATOR; 
+                // //html PNG location prefix
+                // $PNG_WEB_DIR = 'phpqrcode/temp/';
+                // include "phpqrcode/qrlib.php";    
+                // //ofcourse we need rights to create temp dir
+                // if (!file_exists($PNG_TEMP_DIR))
+                //     mkdir($PNG_TEMP_DIR);
+                // $filename = $PNG_TEMP_DIR.'test.png';
+                // $errorCorrectionLevel = 'L';
+                // $matrixPointSize = 4;
+                // if (isset($_REQUEST['data'])) { 
+                //     //it's very important!
+                //     if (trim($_REQUEST['data']) == '')
+                //         die('data cannot be empty! <a href="?">back</a>');
+                //     // user data
+                //     $filename = $PNG_TEMP_DIR.$_REQUEST['data'].'.png';
+                //     QRcode::png($_REQUEST['data'], $filename, $errorCorrectionLevel, $matrixPointSize, 2); 
+
+                // } else {    
+                //     //default data
+                //     echo 'You can provide data in GET parameter: <a href="?data=like_that">like that</a><hr/>';    
+                //     QRcode::png('PHP QR Code :)', $filename, $errorCorrectionLevel, $matrixPointSize, 2);    
+                // }         
+                // //display generated file
+                // echo '<img src=""phpqrcode/"'.$PNG_WEB_DIR.basename($filename).'" />';  
         
  
-
+                
     
                 // $textReplyMessage = "https://www.youtube.com/embed/Yad6t_EgwVw";
                 // $arr_replyData[] = new TextMessageBuilder($textReplyMessage);
             
+
+                $picFullSize = 'https://github.com/anan211139/NECTECinternship/blob/master/app/Http/Controllers/phpqrcode/temp/eiei.png?raw=true';
+                $picThumbnail = 'https://github.com/anan211139/NECTECinternship/blob/master/app/Http/Controllers/phpqrcode/temp/eiei.png?raw=true/240';
+                $arr_replyData[] = new ImageMessageBuilder($picFullSize,$picThumbnail);
+
+
+                //--------REPLY----------
                 $multiMessage =     new MultiMessageBuilder;
                 foreach($arr_replyData as $arr_Reply){
                         $multiMessage->add($arr_Reply);
