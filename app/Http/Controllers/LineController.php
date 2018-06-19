1c6e233dbbe0e448f805833b088b9f752340b7f3
anan <parichaya211139@gmail.com>
2018-06-19 10:02:06 +0700

    try newname

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
-use App\Http\Controllers\phpqrcode\bindings\tcpdf\QRcode;
+use App\Http\Controllers\phpqrcode\bindings\tcpdf\qrcode;
 
 include 'phpqrcode/qrlib.php';
 
