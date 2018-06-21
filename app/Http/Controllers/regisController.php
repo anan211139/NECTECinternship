28b99706345267ac11673c72b67992ff022c47f0
rawatas <whattheshit@outlook.co.th>
2018-06-21 11:35:40 +0700

    edit regisController

--- app/Http/Controllers/regisController.php
+++ app/Http/Controllers/regisController.php
@@ -3,18 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Manager;
 
 class regisController extends Controller
 {
     public function psregis(Request $request){
+        $PRname = "temp";
         $username = $request->input('uname');
         $password = $request->input('psw');
         $repassword = $request->input('repsw');
         $email = $request->input('email');
         if($password == $repassword){
-            return 'inIF';
+            $userresult = Manager::all();
+            for($i = 0;$i<count($userresult);$i++){
+                if($userresult[$i]["UNPR"] == $username){
+                    return 123;
+                }
+                else{
+                    insertuser($username,$password,$email,$PRname);
+                }
+            }  
         }else{
             return redirect('/')->with('failregis','Password and Re-Password not match');
         } 
+    }
+
+    function insertuser($username,$password,$email,$PRname){
+        $message = new Manager;
+        $message->PRname = $PRname;
+        $message->UNPR = $username;
+        $message->PWPR = $password;
+        $message->PREmail = $email;
+        $message->save();
+        return redirect('/')->with('failregis','DONE');
     }
 }
