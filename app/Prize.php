9843212606aa8eefa4e76f3dc34fefa968ce5552
anan <parichaya211139@gmail.com>
2018-06-28 17:37:51 +0700

    edit addess as local_pic

--- app/Prize.php
+++ app/Prize.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prize extends Model
 {
-    protected $fillable = ['sponsor_id', 'name', 'address', 'value', 'point', 'limit', 'type_id'];
+    protected $fillable = ['sponsor_id', 'name', 'local_pic', 'value', 'point', 'limit', 'type_id'];
 }
