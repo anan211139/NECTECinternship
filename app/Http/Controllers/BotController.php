73ca220bc3f581ee7d45c6241fa0cd46102853b1
anan <parichaya211139@gmail.com>
2018-06-28 16:12:27 +0700

    test

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -202,8 +202,12 @@ class BotController extends Controller
                 // $columnTitles = array('foo', 'bar', 'buz','xc');
 
                 // echo $final_prizes;
-                // dd($re_prizes);
-                echo $re_prizes[0]['name'];
+                dd($re_prizes);
+
+
+                //echo $re_prizes[0]['name'];
+
+
                 // foreach ($re_prizes as $value) {
                 //     echo $value;
                 //     // $columnTemplateBuilder = new CarouselColumnTemplateBuilder($result_prizes[$values]['name'], 'description', $imageUrl, [
@@ -213,8 +217,8 @@ class BotController extends Controller
                 //     // array_push($columnTemplateBuilders, $columnTemplateBuilder);
                 // }
 
-                $carouselTemplateBuilder = new CarouselTemplateBuilder($columnTemplateBuilders);
-                $replyData = new TemplateMessageBuilder('รายการ Sponser', $carouselTemplateBuilder);
+                // $carouselTemplateBuilder = new CarouselTemplateBuilder($columnTemplateBuilders);
+                // $replyData = new TemplateMessageBuilder('รายการ Sponser', $carouselTemplateBuilder);
           
             }
             else if($userMessage =="ดู Code"){
