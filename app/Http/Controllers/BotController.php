025938e260bf6790f68c1c27f898c8512beee152
anan <parichaya211139@gmail.com>
2018-06-28 16:23:01 +0700

    echo $re_prizes->name;

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -204,8 +204,10 @@ class BotController extends Controller
                 // echo $final_prizes;
                 //dd($re_prizes);
 
-                echo var_dump($re_prizes);
+                //echo var_dump($re_prizes);
                 //echo $re_prizes[0]['name'];
+
+                echo $re_prizes->name;
 
 
                 // foreach ($re_prizes as $value) {
