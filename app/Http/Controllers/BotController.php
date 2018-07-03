c8b7598145e478fdfe1f46e922b83aea5423fae8
Chayanin <chayanin2007@gmail.com>
2018-07-03 15:48:49 +0700

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -94,6 +94,7 @@ class BotController extends Controller
                 if ($postback_action == "exchange") {
                     list($postback_title, $postback_id) = explode("=", $postback_id_part);
                     $bot->replyMessage($event->getReplyToken(), new TextMessageBuilder(1));
+                    $bot->replyMessage($event->getReplyToken(), new TextMessageBuilder(11));
                     $selected = DB::table('prizes')
                         ->where('id', $postback_id)
                         ->first();
