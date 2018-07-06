a66551c50f34b1b00c22d39ddfd1207b948f6ce4
Chayanin <Chayanin>
2018-07-06 14:24:22 +0700

    debug

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -347,7 +347,7 @@ class BotController extends Controller
                         ->where('group_id', $group_id)
                         ->orderBy('id','DESC')
                         ->first();
-                        $replyData = new TextMessageBuilder($current_log->exam_id);
+                        $replyData = new TextMessageBuilder($group_id);
                         $bot->replyMessage($replyToken,$replyData);
                         continue;
                     $count_quiz = DB::table('logChildrenQuizzes')
