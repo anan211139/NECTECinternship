4dc8df4f2d7578360ce14f1b0ded4203ebd14553
anan <parichaya211139@gmail.com>
2018-07-10 16:23:28 +0700

    echo ">>".$count_quiz ;

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -397,6 +397,7 @@ class BotController extends Controller
         $count_quiz = DB::table('logChildrenQuizzes')
             ->where('group_id', $group_id)
             ->count();
+        echo ">>".$count_quiz ;
         if ($count_quiz % 5 == 0) {
             $count_true = 0;
             $count_quiz_true = DB::table('logChildrenQuizzes')
