974efe55900f5a63a55d491de1e6759afd673d2f
iitim <chayanin2007@gmail.com>
2018-07-16 22:51:19 +0700

    comment dd

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -413,7 +413,7 @@ class BotController extends Controller
                 ->get();
                 //->count();
                 if($count_quiz_true->is_correct===true){
-                    $count_true++;                    dd($count_true);
+                    $count_true++;                    //dd($count_true);
                 }
             if ($count_true >= 3 && $level_id < 3) {
                 $level_id = $level_id + 1;
