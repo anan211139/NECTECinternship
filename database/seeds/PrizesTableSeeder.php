f71388a960f0ba9cfef2a28690238612dc91120e
iitim <chayanin2007@gmail.com>
2018-07-17 16:40:47 +0700

    fix table for each seeder class

--- database/seeds/PrizesTableSeeder.php
+++ database/seeds/PrizesTableSeeder.php
@@ -12,12 +12,11 @@ class PrizesTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('exams')->delete();
-      //insert some dummy records
-      DB::table('exams')->insert(array(
-      array('sponsor_id'=>	1	, 'name' => 'ดินวิทยาศาสตร์'	, 'local_pic' =>'img/sponsor/pmic_p.png'	,'value' =>	50	,'point' =>	100	,'type_id' =>	1),
-      array('sponsor_id'=>	2	, 'name' => 'คอร์สเรียน cmd'	, 'local_pic' =>'img/sponsor/ice_p.png'	,'value' =>	200	,'point' =>	150	,'type_id' =>	1),
-      array('sponsor_id'=>	4	, 'name' => 'คอร์สปลูกต้นไม้'	, 'local_pic' =>'img/sponsor/wari_p.png'	,'value' =>	150	,'point' =>	150	,'type_id' =>	1),
-    ));
+      DB::table('prizes')->delete();
+      DB::table('prizes')->insert(array(
+        array('sponsor_id'=>	1	, 'name' => 'ดินวิทยาศาสตร์'	, 'local_pic' =>'img/sponsor/pmic_p.png'	,'value' =>	50	,'point' =>	100	,'type_id' =>	1),
+        array('sponsor_id'=>	2	, 'name' => 'คอร์สเรียน cmd'	, 'local_pic' =>'img/sponsor/ice_p.png'	,'value' =>	200	,'point' =>	150	,'type_id' =>	1),
+        array('sponsor_id'=>	4	, 'name' => 'คอร์สปลูกต้นไม้'	, 'local_pic' =>'img/sponsor/wari_p.png'	,'value' =>	150	,'point' =>	150	,'type_id' =>	1),
+      ));
     }
 }
--- database/seeds/SponsorTableSeeder.php
+++ database/seeds/SponsorTableSeeder.php
@@ -12,9 +12,8 @@ class SponsorTableSeeder extends Seeder
      */
     public function run()
     {
-      DB::table('exams')->delete();
-      //insert some dummy records
-      DB::table('exams')->insert(array(
+      DB::table('sponsors')->delete();
+      DB::table('sponsor')->insert(array(
         array('name' =>	'iammmickky', 'local_pic'=>	'img/sponsor/pmic_p.png', 'phone' =>	'099'),
         array('name' =>	'ice', 'local_pic'=>	'img/sponsor/ice_p.png', 'phone' =>	'086'),
         array('name' =>	'anan', 'local_pic'=>	'gooo!', 'phone' =>	'90'),
