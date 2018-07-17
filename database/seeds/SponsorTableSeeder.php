b78ec9455225c231d1665f213fc79fa24c1ebe0a
iitim <chayanin2007@gmail.com>
2018-07-17 16:31:43 +0700

    fix seeder table

--- database/seeds/SponsorTableSeeder.php
+++ database/seeds/SponsorTableSeeder.php
@@ -14,10 +14,10 @@ class SponsorTableSeeder extends Seeder
       DB::table('exams')->delete();
       //insert some dummy records
       DB::table('exams')->insert(array(
-        array('name' =>	'iammmickky'	,'local_pic'=>	'img/sponsor/pmic_p.png'	,'phone' =>	'099',
-        array('name' =>	'ice'	,'local_pic'=>	'img/sponsor/ice_p.png'	,'phone' =>	'086',
-        array('name' =>	'anan'	,'local_pic'=>	'gooo!'	,'phone' =>	'90',
-        array('name' =>	'wari'	,'local_pic'=>	'img/sponsor/wari_p.png'	,'phone' =>	'4400',
+        array('name' =>	'iammmickky', 'local_pic'=>	'img/sponsor/pmic_p.png', 'phone' =>	'099'),
+        array('name' =>	'ice', 'local_pic'=>	'img/sponsor/ice_p.png', 'phone' =>	'086'),
+        array('name' =>	'anan', 'local_pic'=>	'gooo!', 'phone' =>	'90'),
+        array('name' =>	'wari', 'local_pic'=>	'img/sponsor/wari_p.png', 'phone' =>	'4400'),
       ));
     }
 }
