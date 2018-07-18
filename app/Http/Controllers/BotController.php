be6f5fe652032fb350ee2fe5d3e91ec4bd217553
iitim <chayanin2007@gmail.com>
2018-07-18 13:51:11 +0700

    === to ==

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -661,7 +661,7 @@ class BotController extends Controller
         // $group_count = DB::table('results')
         //     ->where('group_id',$group_true->id)
         //     ->count();
-        if($group_true === null){
+        if($group_true == null){
             $concat_result = "ยังไม่มีคะแนนสอบชุดล่าสุด";
         }
         else{
