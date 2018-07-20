6849981da2c3080e2ab8c6ccaabd1841c886e3a3
parewari <maroengsit.pwr@gmail.com>
2018-07-20 16:55:59 +0700

    voyyyy

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -367,7 +367,7 @@ class BotController extends Controller
                   if ($checkIMG == 0) {
                       $response = $bot->getProfile($userId);
                       echo "RES";
-                      if (1) {
+                      if ($response->isSucceeded()) {
                           echo "TRUE";
                           $profile = $response->getJSONDecodedBody();
                           $url_img = $profile['pictureUrl'];
