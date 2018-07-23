e6f4d92d2d55eb069adf48ac8cba5e570decf168
anan <parichaya211139@gmail.com>
2018-07-23 13:27:24 +0700

    test without profile

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -393,13 +393,13 @@ class BotController extends Controller
                     // }
                     
                     $multiMessage = new MultiMessageBuilder;
-                    $response = $bot->getProfile($userId);
-                    $stdprofile = $response->getJSONDecodedBody();
+                    // $response = $bot->getProfile($userId);
+                    // $stdprofile = $response->getJSONDecodedBody();
                     // $arr_replyData[] = new TextMessageBuilder("สวัสดีจ้านี่พี่หมีเอง\nยินดีที่เราได้เป็นเพื่อนกันนะน้อง ".$stdprofile['displayName']);
                     $arr_replyData[] = new TextMessageBuilder("ก่อนเริ่มบทเรียน ควรดูคลิปวิธีการใช้งานด้านล่างนี้ก่อนนะ");
                     $arr_replyData[] = new TextMessageBuilder("เอาล่ะ! ถ้าพร้อมแล้ว เรามาเลือกวิชาแรกที่จะทำข้อสอบกันเถอะ");
                     $imageMapUrl = 'https://github.com/anan211139/NECTECinternship/blob/master/img/final_subject.png?raw=true';
-                    $arr_replyData[] = new TextMessageBuilder($profile['pictureUrl']);
+                    // $arr_replyData[] = new TextMessageBuilder($profile['pictureUrl']);
                     $arr_replyData[] = new ImagemapMessageBuilder(
                         $imageMapUrl,
                         "รายการวิชา",
