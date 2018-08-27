d09ada99bbd4e3bc7c748dc35588c723508298be
anan <parichaya211139@gmail.com>
2018-08-27 15:21:59 +0700

    cooment dd

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -550,8 +550,8 @@ class BotController extends Controller
                         $exam_new = DB::table('Exam_New')
                             ->where('id', 1)
                             ->get();
-                        dd($exam_new);
-                        $check_pic_exam = $exam_new ->local_pic;
+                        //dd($exam_new);
+                        $check_pic_exam = $exam_new->local_pic;
                             if ($check_pic_exam === null){
                                 echo "ไม่มีรูป";
                             }else{
