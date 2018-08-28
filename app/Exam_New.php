5db557ac024ed6fbfc96436e1e86db79d5ebe55d
anan <parichaya211139@gmail.com>
2018-08-28 09:49:12 +0700

    edit model

--- app/Exam_New.php
+++ app/Exam_New.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exam_New extends Model
 {
-    protected $fillable = ['chapter_id', 'level_id', 'question' , 'choice_a' , 'choice_b', 'choice_c', 'choice_d','local_pic', 'answer', 'principle_id'];
+    //protected $fillable = ['chapter_id', 'level_id', 'question' , 'choice_a' , 'choice_b', 'choice_c', 'choice_d','local_pic', 'answer', 'principle_id'];
+    protected $table = 'Exam_New';
 }
