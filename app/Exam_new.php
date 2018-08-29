ad1eace5e6a847fd7f5ec008101c183b0efb26dc
anan <parichaya211139@gmail.com>
2018-08-29 16:33:47 +0700

    edit model

---
+++ app/Exam_new.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exam_new extends Model
+{
+     protected $fillable = ['chapter_id', 'level_id', 'question' , 'choice_a' , 'choice_b', 'choice_c', 'choice_d','local_pic', 'answer', 'principle_id'];
+     //rotected $table = 'Exam_News';
+}
